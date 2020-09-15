ecddccdcb3c612f4859ecaf6f3c9e5533b0dcd0f
Manjunath264 <71065692+Manjunath264@users.noreply.github.com>
2020-09-15 12:45:05 +0530

    similarinput

---
+++ sample.java
@@ -0,0 +1,8 @@
+public class HelloGuys{
+public static void main(String [] args){
+System.out.pintln("Hi manjunath");
+System.out.pintln("Hi mk");
+System.out.pintln("Hi keeajju");
+System.out.pintln("Hello keeajju");
+}
+}
